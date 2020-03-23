7b035ab4118d8e9a8a1d70ab5a2a5bd74352a7f8
wsy <wsy1983wsy@163.com>
2020-03-23 22:23:15 +0800

    增加打印

--- First/index.js
+++ First/index.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron')
 
 function createWindow() {
+    //BrowserWindow代表一个window，
     let win = new BrowserWindow({
         width: 800,
         height: 600,
@@ -10,12 +11,14 @@ function createWindow() {
     })
     //加载index.html
     win.loadFile('index.html')
+    win.on("close", () => {
+        console.log('closed')
+        win = null
+    })
 
     // 打开开发者工具
     win.webContents.openDevTools()
 }
-
-app.whenReady().then(createWindow)
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -24,6 +27,7 @@ app.whenReady().then(createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
+    console.log('all window closed')
     // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
     // 否则绝大部分应用及其菜单栏会保持激活。
     if (process.platform !== 'darwin') {
@@ -32,6 +36,7 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
+    console.log('activate')
     // 在macOS上，当单击dock图标并且没有其他窗口打开时，
     // 通常在应用程序中重新创建一个窗口。
     if (BrowserWindow.getAllWindows().length === 0) {
