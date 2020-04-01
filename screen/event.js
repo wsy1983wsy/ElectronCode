aaf1514938b291d353e374eaeb85b512b2cede46
wsy <wsy1983wsy@163.com>
2020-04-01 11:12:24 +0800

    screen

---
+++ screen/event.js
@@ -0,0 +1,19 @@
+/*
+获取屏幕尺寸和鼠标绝对坐标
+*/
+
+const electron = require('electron');
+const remote = electron.remote;
+const { screen } = remote
+function clickScreen() {
+    const win = remote.getCurrentWindow();
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+    console.log('width:' + width);
+    console.log('height:' + height);
+    win.setSize(width, height, true);
+    win.setPosition(0, 0);
+
+    // 获取鼠标当前的绝对坐标
+    console.log('x:' + screen.getCursorScreenPoint().x);
+    console.log('y:' + screen.getCursorScreenPoint().y);
+}
